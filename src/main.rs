02ea36c065fcc9342913b4d9297d518667aa01b2
hagaitski <i.haga629@gmail.com>
2022-02-09 23:17:44 +0900

    fix: path join

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{fs::{File, read_to_string}, io::Write};
 use clap::Parser;
+use std::path::Path;
 
 use git_external_files_lib::{input::DependenciesJson, fetch::fetch_row_content};
 
@@ -18,11 +19,12 @@ struct Args {
 #[tokio::main]
 async fn main() -> Result<(),Box<dyn std::error::Error>>{
     let args = Args::parse();
+    let output = Path::new(args.output.as_str());
     let inputs = read_to_string(args.config.as_str())?;
     let dep_json : DependenciesJson = serde_json::from_str(inputs.as_str())?;
     for (name,dependency) in dep_json.dependencies {
         let res = fetch_row_content(dependency).await?;
-        let mut file = File::create(args.output.to_string() + "/" + &name)?;
+        let mut file = File::create(output.join(name.as_str()))?;
         file.write_all(&res.as_bytes())?;
         file.flush()?;
     } 
